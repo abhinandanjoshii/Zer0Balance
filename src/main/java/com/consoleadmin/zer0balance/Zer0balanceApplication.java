bbbb0fd66add777554cd01016d463ac45de96a96
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-07-17 23:45:49 +0530

    backend config

---
+++ src/main/java/com/consoleadmin/zer0balance/Zer0balanceApplication.java
@@ -0,0 +1,13 @@
+package com.consoleadmin.zer0balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zer0balanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zer0balanceApplication.class, args);
+	}
+
+}
