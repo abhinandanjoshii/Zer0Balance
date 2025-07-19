2c03132ff10b91a2675ede963234f9b3bd903b49
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-07-19 20:07:15 +0530

    v1.0 feature -> categories for user

---
+++ src/main/java/com/consoleadmin/zer0balance/service/CategoryService.java
@@ -0,0 +1,69 @@
+package com.consoleadmin.zer0balance.service;
+
+import com.consoleadmin.zer0balance.dto.CategoryDTO;
+import com.consoleadmin.zer0balance.entity.CategoryEntity;
+import com.consoleadmin.zer0balance.entity.ProfileEntity;
+import com.consoleadmin.zer0balance.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final ProfileService profileService;
+    private final CategoryRepository categoryRepository;
+
+
+    public CategoryDTO saveCategory(CategoryDTO categoryDTO) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        if(categoryRepository.existsByNameAndProfileId(categoryDTO.getName(), profile.getId())) {
+            throw new RuntimeException("Category already exists");
+        }
+
+        CategoryEntity newCategory = toEntity(categoryDTO,profile);
+        newCategory = categoryRepository.save(newCategory);
+        return toDTO(newCategory);
+    }
+
+    public List<CategoryDTO> getCategoriesForCurrentUser() {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<CategoryEntity> categories = categoryRepository.findByProfileId(profile.getId());
+        return categories.stream().map(this::toDTO).toList();
+    }
+
+    public List<CategoryDTO> getCategoriesByTypeForCurrentUser(String type) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<CategoryEntity> entities = categoryRepository.findByTypeAndProfileId(type, profile.getId());
+        return entities.stream().map(this::toDTO).toList();
+    }
+
+    // helper methods (core)
+
+    private CategoryEntity toEntity(CategoryDTO categoryDTO, ProfileEntity profile) {
+        return CategoryEntity.builder()
+                .name(categoryDTO.getName())
+                .icon(categoryDTO.getIcon())
+                .profile(profile)
+                .type(categoryDTO.getType())
+                .build();
+    }
+
+    private CategoryDTO toDTO(CategoryEntity entity) {
+        return CategoryDTO.builder()
+                .id(entity.getId())
+                .profileId(entity.getProfile() != null ? entity.getProfile().getId(): null)
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .type(entity.getType())
+                .build();
+    }
+}
